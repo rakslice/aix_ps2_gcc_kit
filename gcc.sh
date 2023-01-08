5faddde6a80a639ec4f51ca754596b78c0250bb9
rakslice <rakslice@gmail.com>
2023-01-07 16:42:00 -0800

    cleanup

--- gcc.sh
+++ gcc.sh
@@ -9,17 +9,8 @@ cd build-gcc
 
 cp ../libgcc1.a .
 
-#../gcc-2.6.3/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
-#../gcc-2.6.3/configure --target=$TARGET --prefix="$PREFIX" --enable-languages=c,c++ --without-headers --host=i486-unknown-linux
-
-#../gcc-2.7.2.3/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers --host=i486-unknown-linux
-
 ../gcc-2.7.2.3/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ \
 --with-sysroot="$PREFIX/sysroot" --host=i486-unknown-linux --with-headers
-#make all-gcc
-#make all-target-libgcc
-#make install-gcc
-#make install-target-libgcc
 make LANGUAGES=c "$@"
 make LANGUAGES=c install
 
