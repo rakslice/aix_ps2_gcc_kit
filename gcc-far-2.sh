65958fb15cbb02f6bcd1b6eeb725c61136ab8c6f
rakslice <rakslice@gmail.com>
2023-01-07 16:39:21 -0800

    cleanup

--- gcc-far-2.sh
+++ gcc-far-2.sh
@@ -13,18 +13,12 @@ cd build-gcc
 
 cp ../libgcc1.a .
 
-#OLD_PREFIX="$PREFIX"
-
 export PREFIX="$PREFIX/../far"
 
 ../gcc-2.7.2.3/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ \
 --with-sysroot="$PREFIX/sysroot" --host=$TARGET --with-headers --build=i486-unknown-linux
 
-#make all-gcc
-#make all-target-libgcc
-#make install-gcc
-#make install-target-libgcc
-args="LANGUAGES=c GCC_FOR_TARGET=i386-ibm-aix-gcc CC=i386-ibm-aix-gcc HOST_CC=gcc HOST_ALLOCA= HOST_CLIB= HOST_PREFIX=../build-gcc.1/ "
+args="LANGUAGES=c GCC_FOR_TARGET=i386-ibm-aix-gcc CC=i386-ibm-aix-gcc HOST_CC=gcc HOST_ALLOCA= HOST_CLIB= "
 make $args "$@"
 make $args install
 
