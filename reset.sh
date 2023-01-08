9d965fa0e61750329c3bcf9206acc55b7a089bd9
rakslice <rakslice@gmail.com>
2023-01-07 16:35:18 -0800

    output files missing from reset

--- reset.sh
+++ reset.sh
@@ -4,5 +4,5 @@ set -e -x
 ./cleanup_chroots
 
 sudo rm -rf cdrom_chroot rh5_chroot/ build-binutils/ build-binutils.1/ build-gcc/ build-gcc.1/ \
-    sysroot/ aixinclude/ *.done binutils-2.9.1/ gcc-2.7.2.3/
+    sysroot/ aixinclude/ *.done binutils-2.9.1/ gcc-2.7.2.3/ crossnative.tar.gz crossnative*.dsk
 
