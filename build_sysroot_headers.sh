8891912ba32cd98cfb9f22c58baaee596cdca1b8
rakslice <rakslice@gmail.com>
2023-01-15 22:54:03 -0800

    we do not want the placeholder float.h in the override headers set

--- build_sysroot_headers.sh
+++ build_sysroot_headers.sh
@@ -15,6 +15,8 @@ mkdir sysroot_headers
 cp -a aixinclude/usr/include sysroot_headers
 cp -a /tmp/rh5_chroot/root/opt/cross/lib/gcc-lib/i386-ibm-aix/$GCC_VER/include sysroot_headers
 
+rm sysroot_headers/include/float.h
+
 output=sysroot_headers-$GCC_VER.tar.gz
 
 if [ -f "$output" ]; then
